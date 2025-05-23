86bb76891ecc9a8020feda7bda555be13a81c98b
PentaIonic <johnfernandojorvina@gmail.com>
2025-05-24 01:07:48 +0800

    Refactor login and search panel structure; remove obsolete files and update paths for loading modules

--- src/scripts/modules/login/login.js
+++ src/scripts/modules/login/login.js
@@ -8,6 +8,6 @@ function loadPortalIndex() {
       console.error("Failed to load portal:", xhr.status);
     }
   };
-  xhr.open("GET", "./src/modules/login.html", true);
+  xhr.open("GET", "./src/modules/login/login.html", true);
   xhr.send();
 }
