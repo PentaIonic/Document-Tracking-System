a2c52812d0fb49cfe9e14a71022865c532ed4ff4
John Fernando Jorvina <158750035+PentaIonic@users.noreply.github.com>
2025-06-09 23:44:05 +0800

    Temp fix

--- src/scripts/components/navbar.js
+++ src/scripts/components/navbar.js
@@ -86,7 +86,7 @@ function loadNavAdmin() {
 
       setupMenuToggle();
       setupNotificationWindowAdminOnly();
-      insertDisplayNameAdminPanel();
+      insertDisplayName();
       loadNotifications();
       insertRole();
     } else {
