5e8259b8203f00161c2a7490617a818139118725
PentaIonic <johnfernandojorvina@gmail.com>
2025-06-09 19:30:19 +0800

    fix: Correct asset paths in navigation and search document scripts

--- src/scripts/pages/search/searchDocument.js
+++ src/scripts/pages/search/searchDocument.js
@@ -1,7 +1,7 @@
 // This function serves to redirect the user to results page with the value inputted in the search box.
 function searchDocument(codeDocument) {
   const params = new URLSearchParams({ codeDocument });
-  window.location.href = `../../search/results.html?${params.toString()}`;
+  window.location.href = `../search/results.html?${params.toString()}`;
 }
 
 // If the user clicks or enters the search field, this function will be called.
