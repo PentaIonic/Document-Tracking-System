595f3df27b12b55d5993d37090653c87f144f3eb
John Fernando Jorvina <158750035+PentaIonic@users.noreply.github.com>
2025-06-28 14:00:13 +0800

    Update connection.php

--- src/scripts/components/database/connection.php
+++ src/scripts/components/database/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbhost = "db.be-mons1.bengt.wasmernet.com:3306";
-$dbuser = "f7eedbb474f58000df79fd1d33cc";
-$dbpass = "0685f7ee-dbb5-7157-8000-19d4bb55126a";
-$dbname = "dbk68QB8CHqkc9J5cG5Rau9o";
+$dbhost = "localhost:3306";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "doctrax";
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
